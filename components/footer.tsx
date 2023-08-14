af4ebe8e4fee6c1069fcd1ebc261c207cba169e6
genesluna <genesluna@gmail.com>
2023-08-14 00:36:30 -0300

    Added footer component

---
+++ components/footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="px-4 mb-10 text-center text-gray-500">
+      <small className="block mb-2 text-xs">&copy; 2023 Genes Luna. All rights reserved.</small>
+      <p className="text-xs">
+        Built with React & Next.js (App Router & Server Actions), TypeScript, Tailwind CSS, Framer Motion, React Email &
+        Resend.
+      </p>
+    </footer>
+  );
+}
